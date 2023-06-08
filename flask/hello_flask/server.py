2a631ba5d91ec94c0f95beca910109ffc8962c46
ryanjnam99 <ryanjnam@yahoo.com>
2023-06-08 11:44:26 -0700

    added checkerboard

--- flask/hello_flask/server.py
+++ flask/hello_flask/server.py
@@ -23,6 +23,12 @@ def play(num, color):
     return render_template("playground3.html", times=num, color=color)
 # This is the code for playground assignment
 
+# @app.route('/<int:num>/<int:num2>/<string:one>/<string:two>')
+# def checkerboard(num,num2,one,two):
+#     return render_template("checkerboard.html", row=num, column=num2, color=one, color2=two)
+@app.route('/<int:x>/<int:y>/<color1>/<color2>')
+def row_col_two(x,y,color1,color2):
+    return render_template("checkerboard.html",row=x,col=y,color_one=color1,color_two=color2)
     
 # @app.route('/success')
 # def success():
