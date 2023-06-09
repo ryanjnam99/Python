a9136830369b1679dd0b4d676558e9cb49e62c98
ryanjnam99 <ryanjnam@yahoo.com>
2023-06-09 14:34:48 -0700

    added counter

---
+++ flask/hello_flask/serverSample.py
@@ -0,0 +1,63 @@
+from flask import Flask, render_template, request, redirect, session
+# added request and redirect
+app = Flask(__name__)
+app.secret_key = 'keep it secret, keep it safe' # set a secret key for security purposes
+
+# our index route will handle rendering our form
+@app.route('/')
+def index():
+    return render_template("form.html")
+
+# added request and redirect
+            
+
+
+# @app.route('/users', methods=['POST'])
+# def create_user():
+#     print("Got Post Info")
+#     print(request.form)
+#     return redirect('/')
+
+# @app.route('/users', methods=['POST'])
+# def create_user():
+#     print("Got Post Info")
+#     print(request.form)
+#     name = request.form['name']
+#     email = request.form['email']
+#     return redirect("/show")	
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    print("Got Post Info")
+    # Here we add two properties to session to store the name and email
+    # session['username'] = request.form['name']
+    # session['useremail'] = request.form['email']
+    session['usernum'] = request.form['num']
+    return redirect('/show')
+
+# @app.route('/show')
+# def show_user():
+#     return render_template('show.html', name_on_template=session['username'], email_on_template=session['useremail'])
+
+
+    
+# adding this method
+@app.route("/show")
+def show_user():
+    print("Showing the User Info From the Form")
+    print(request.form)
+    return render_template("show.html")
+
+
+
+    
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
