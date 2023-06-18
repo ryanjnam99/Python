e04663f5837ab6ae26a5e9e6d033b9fdcbea7fd9
ryanjnam99 <ryanjnam@yahoo.com>
2023-06-17 22:44:33 -0700

    added login page

---
+++ flask_mysql/registration/flask_app/controllers/registration_controller.py
@@ -0,0 +1,71 @@
+from flask_app import app
+from flask import render_template, request, redirect, session
+from flask_app.models.registrations import Registration
+from flask import flash
+from flask_bcrypt import Bcrypt 
+bcrypt = Bcrypt(app)
+
+
+# our index route will handle rendering our form
+@app.route('/')
+def index():
+    return render_template("registration.html")
+
+@app.route('/create/user', methods=['POST'])
+def register():
+    pw_hash = bcrypt.generate_password_hash(request.form['password']).decode('utf-8')
+    print(pw_hash)
+    # data = {
+    #     "email" : request.form['email'],
+    #     "password": pw_hash
+    # }
+    if not Registration.validate_user(request.form):
+        return redirect('/')
+    if not Registration.validate_email(request.form):
+        return redirect('/')
+    # newUser_data = {
+    #     'email': request.form['email'],
+    #     'password': pw_hash
+    # }
+    user_id = Registration.save(request.form)
+    session['id'] = user_id
+    return redirect(f"/welcome/{user_id}")
+
+@app.route('/login/page')
+def go_to_login():
+    return render_template("login.html")
+
+@app.route("/login/user", methods=['POST'])
+def login():
+    data = {
+        'email': request.form['email']
+    }
+    user_in_db = Registration.get_by_email(data)
+    pw_hash = bcrypt.generate_password_hash(request.form['password']).decode('utf-8')
+    if not user_in_db:
+        flash("Invalid Email/Password")
+        return redirect("/login/page")
+    if not bcrypt.check_password_hash(pw_hash, user_in_db.password):
+        flash("Invalid Email/Password")
+        return redirect('/login/page')
+    session['id'] = user_in_db.id
+    return redirect(f"/welcome/{user_in_db.id}")
+
+@app.route("/welcome/<int:user_id>")
+def welcome(user_id):
+    data = {
+        'id': user_id
+    }
+    one_user = Registration.get_user(data)
+    return render_template("welcome.html",user=one_user)
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect('/')
+    
+
+
+
+
+
