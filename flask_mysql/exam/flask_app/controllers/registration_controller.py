47745e0b37df7ebbbf2ecbeb5171d58882e8a895
ryanjnam99 <ryanjnam@yahoo.com>
2023-06-20 14:17:47 -0700

    Selective editing

--- flask_mysql/exam/flask_app/controllers/registration_controller.py
+++ flask_mysql/exam/flask_app/controllers/registration_controller.py
@@ -1,34 +1,30 @@
 from flask_app import app
 from flask import render_template, request, redirect, session
-from flask_app.models.persons import Person
-from flask_app.models.recipes import Recipe
+from flask_app.models.registrations import Registration
 from flask import flash
 from flask_bcrypt import Bcrypt 
 bcrypt = Bcrypt(app)
 
 
-
+# our index route will handle rendering our form
 @app.route('/')
 def index():
-    return render_template("person.html")
-
-# Sends to the registration page
+    return render_template("registration.html")
 
 @app.route('/create/user', methods=['POST'])
 def register():
-    if not Person.validate_user(request.form):
+    if not Registration.validate_user(request.form):
         return redirect('/')
-    if not Person.validate_email(request.form):
+    if not Registration.validate_email(request.form):
         return redirect('/')
     pw_hash = bcrypt.generate_password_hash(request.form['password']).decode('utf-8')
     data = {
         **request.form, 
         "password": pw_hash
     }
-    user_id = Person.save(data)
+    user_id = Registration.save(data)
     session['id'] = user_id
     return redirect("/welcome")
-#  Creates users
 
 @app.route('/login/page')
 def go_to_login():
@@ -39,7 +35,7 @@ def login():
     data = {
         'email': request.form['email']
     }
-    user_in_db = Person.get_by_email(data)
+    user_in_db = Registration.get_by_email(data)
     if len(request.form['password']) < 4:
         flash("Invalid Email/Password")
         return redirect("/login/page")
@@ -58,10 +54,8 @@ def welcome():
     data = {
         'id': session['id']
     }
-    one_user = Person.get_person(data)
-    all_recipes = Recipe.get_all_recipes()
-    return render_template("welcome.html",user=one_user, recipes=all_recipes)
-
+    one_user = Registration.get_user(data)
+    return render_template("welcome.html",user=one_user)
 
 @app.route("/logout")
 def logout():
---
+++ flask_mysql/exam/flask_app/models/registrations.py
@@ -0,0 +1,71 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask import flash
+import re
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$') 
+
+
+class Registration:
+    DB = "registration_schema"
+    def __init__(self, data):
+        self.id = data['id']
+        self.name = data['name']
+        self.email = data['email']
+        self.password = data['password']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+    @classmethod
+    def save(cls, data):
+        query = """INSERT into registrations (name, email, password) 
+        VALUES (%(name)s, %(email)s, %(password)s);"""
+        return connectToMySQL("registration_schema").query_db(query,data)
+
+    @classmethod
+    def get_user(cls, data):
+        query = "SELECT * FROM registrations WHERE id = %(id)s;"
+        results = connectToMySQL("registration_schema").query_db(query,data)
+        return cls(results[0])
+
+    @classmethod
+    def get_by_email(cls,data):
+        query = "SELECT * FROM registrations WHERE email = %(email)s;"
+        results = connectToMySQL("registration_schema").query_db(query,data)
+        # Didn't find a matching user
+        if len(results) < 1:
+            return False
+        return cls(results[0])
+
+    
+    
+    @staticmethod
+    def validate_user(user):
+        is_valid = True
+        if len(user['name']) < 2:
+            flash("Name must be at least 2 characters.")
+            is_valid = False
+        if not EMAIL_REGEX.match(user['email']):
+            flash("Invalid email address!")
+            is_valid = False
+        if len(user['password']) < 8:
+            flash("Password must be at least 8 characters.")
+            is_valid = False
+        if user['password'] != user['confirm_password']:
+            flash("Password does not match")
+            is_valid = False
+        return is_valid
+
+    @staticmethod
+    def validate_email(user):
+        query = "SELECT * FROM registrations WHERE email = %(email)s;"
+        results = connectToMySQL("registration_schema").query_db(query,user)
+        print(results)
+        is_valid = True
+        if len(results) >= 1:
+            flash("Email already entered!")
+            is_valid = False
+        return is_valid
+
+
+
+
+
